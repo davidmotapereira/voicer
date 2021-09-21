c074654b4fde89a2026339d03ebb1776f8abbcde
davidmotapereira <69486608+davidmotapereira@users.noreply.github.com>
2021-09-21 15:04:31 +0200

    Stat:0

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -30,13 +33,29 @@ def take_request():
 
 
 def running_slave():
-    #engine.say('Hello Master, what can i do for you?')
     command = take_request()
     print(command)
-    if 'play' in command:V
+    if 'play' in command:
         song = command.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M')
+        talk('The current time is, ' + time)
+    elif 'search for' in command:
+        search = command.replace('search for', '')
+        info = wikipedia.summary(search, 1)
+        print(info)
+        talk(info)
+    elif 'gay' in command:
+        talk('Why are you gay?')
+        talk('You are gay!')
+    elif 'joke' in command:
+        joke = pyjokes.get_joke()
+        talk(joke)
+        print(joke)
+    else:
+        talk('Please repeat.')
 
-
+#while True:
 running_slave()
